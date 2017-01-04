af977a74b28fd9a1e80140a59ac4e3f5730194cc
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2017-01-04 12:02:08 -0200

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws-instance-debugger',
-    version='0.3.7',
+    version='0.3.9',
     description='CLI to debug AWS instances',
     packages=find_packages(),
     license='MIT',
